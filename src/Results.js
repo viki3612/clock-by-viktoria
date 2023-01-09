35cb6aa76a22914c70c9a66d19960c97bd2067b0
viki3612 <viktorianikolova95@gmail.com>
2023-01-09 16:35:06 +0000

    css added

--- src/Results.js
+++ src/Results.js
@@ -8,34 +8,42 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Result">
-        <h2 className="time-output mb-3">
-          {props.results.data.datetime.hour_24_wilz}:
-          {props.results.data.datetime.minutes}{" "}
-          {props.results.data.datetime.hour_am_pm}
-        </h2>{" "}
-        <div className="gmt mb-3">
-          GMT {""}({props.results.data.datetime.offset_gmt}){" "}
+        <div className="time-info">
+          <h2 className="main-output">
+            {props.results.data.datetime.hour_24_wilz}:
+            {props.results.data.datetime.minutes}{" "}
+            {props.results.data.datetime.hour_am_pm}
+          </h2>{" "}
         </div>
+
         <div className="row additional-info">
           <div className="col location-box">
-            <img src={location} className="img-fluid" alt="icon" width="175" />
-            <div className="location-output mt-3 mb-3">
+            <img src={location} className="img-fluid" alt="icon" width="150" />
+            <div className="location-output mb-3">
               {props.results.data.timezone.id}
+            </div>
+            <div className="country-name">
+              {props.results.data.timezone.country_name}
             </div>
           </div>
           <div className="col date-box">
-            <img src={date} className="img-fluid" alt="icon" width="175" />
-            <div className="date-output mt-3 mb-3">
-              {props.results.data.datetime.day_abbr}, {""}
+            <img src={date} className="img-fluid" alt="icon" width="150" />
+            <div className="date-output mb-3">
+              {props.results.data.datetime.day_abbr}
+            </div>
+            <div className="date">
               {props.results.data.datetime.day}
               {""} {props.results.data.datetime.month_full} {""}
               {props.results.data.datetime.year}{" "}
             </div>
           </div>
           <div className="col timezone-box">
-            <img src={clock} className="img-fluid" alt="icon" width="175" />
-            <div className="timezone-output mt-3 mb-3">
+            <img src={clock} className="img-fluid" alt="icon" width="150" />
+            <div className="timezone-output mb-3">
               {props.results.data.datetime.offset_tzfull}
+            </div>
+            <div className="gmt">
+              GMT {""}({props.results.data.datetime.offset_gmt}){" "}
             </div>
           </div>
         </div>
